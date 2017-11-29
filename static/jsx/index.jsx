f51eaa56c091a70954074fc3cdfd2c2e3dd94afd
Jennifer <jennifer.gd42@gmail.com>
2017-11-29 13:03:58 -0800

    React components work

--- static/jsx/index.jsx
+++ static/jsx/index.jsx
@@ -1,27 +1,194 @@
 "use strict";
 
+class NavTab extends React.Component {
+    constructor (props) {
+        super(props);
+        this.active = this.props.active;
+    }
 
-class Restaurant extends React.Component {
-    render() {
-        const name = this.props.name;
-        return <h1>{ name }</h1>;
+    render () {
+        return (
+            <li role="presentation" className={ this.props.active }>
+                <a href="/?building={ this.props.building }">{ this.props.building } Sutter</a>
+            </li>
+        );
     }
 }
 
+class AllNavTabs extends React.Component {
+    render () {
+        return (
+            <ul className="nav nav-tabs">
+                <NavTab building="683" active="active" />
+                <NavTab building="450" />
+            </ul>
+        );
+    }
+}
+
+class RestaurantOtherInfo extends React.Component {
+    render () {
+        return (
+            <div className="other-info">
+                Price: { "$".repeat(this.props.dollarSigns) } <br /> 
+                Address: { this.props.address }<br />
+                Distance from Hackbright: { this.props.distanceAway } <br />
+                Time to walk from Hackbright: { this.props.timeAway } <br />
+            </div>
+        )
+    }
+}
+
+class RestaurantCategories extends React.Component {
+    render () {
+        return (
+            <div className="category-list">
+                { this.props.categories.join(", ") }
+            </div>
+        )
+    }
+}
+
+class RestaurantImage extends React.Component {
+    render () {
+        return (
+            <div className="col-xs-12 col-md-12">
+                <img className="img-responsive img-rounded thumbnail restaurant-img" 
+                     src={ this.props.img } />
+            </div>
+        );
+    }
+}
+
+class RestaurantHeading extends React.Component {
+    render () {
+        return (
+            <h4 className="rest-name"> 
+                <a href="{ this.props.url }"> 
+                    { this.props.name + "<--- PROPSSSS"}
+                </a> 
+            </h4>
+        );
+    }
+}
+
+class Restaurant extends React.Component {
+
+    render() {
+        const name = this.props.name;
+        const categories = ["apple", "berry"]
+
+        return (
+            <div className="col-xs-6 col-xs-offset-1 col-md-4">
+                <div className="container well well-lg">
+                    <RestaurantHeading name={ this.props.name } url={ this.props.yelpURL }/>
+                    <div className="row info">
+                        <div className="col-xs-12 col-md-12">
+                            <RestaurantCategories categories={ this.props.categories } />
+                            <RestaurantOtherInfo dollarSigns={ this.props.dollarSigns }
+                                                 address={ this.props.address }
+                                                 distanceAway={ this.props.distanceAway }
+                                                 timeAway={ this.props.timeAway }/>
+                            <RestaurantImage img={ this.props.img } />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class RestaurantList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // FIXME: Get the state via AJAX?
+        this.state = {
+            "683": [
+            {
+              "address": "704 Sutter St", 
+              "categories": [
+                "Japanese", 
+                "Sushi Bars", 
+                "Asian Fusion"
+              ], 
+              "distanceAway": "249.0 ft", 
+              "dollarSigns": 2, 
+              "img": "https://s3-media2.fl.yelpcdn.com/bphoto/896N7UXBJk9rzG9BFZY1rw/o.jpg", 
+              "name": "Sanraku", 
+              "timeAway": "1.0 min", 
+              "yelpURL": "https://www.yelp.com/biz/sanraku-san-francisco-2?adjust_creative=oH33mM4Gu5dN2sYrEpJZ-w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=oH33mM4Gu5dN2sYrEpJZ-w"
+            }, 
+            {
+              "address": "679 Sutter St", 
+              "categories": [
+                "Mexican"
+              ], 
+              "distanceAway": "7.0 ft", 
+              "dollarSigns": 2, 
+              "img": "https://s3-media3.fl.yelpcdn.com/bphoto/0iXpzQAzmGR246ZKOnQacg/o.jpg", 
+              "name": "Matador", 
+              "timeAway": "1.0 min", 
+              "yelpURL": "https://www.yelp.com/biz/matador-san-francisco-2?adjust_creative=oH33mM4Gu5dN2sYrEpJZ-w&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=oH33mM4Gu5dN2sYrEpJZ-w"
+            }
+        ]};
+    }
+
+    render () {
+        let restaurants = []
+
+        for (let each of this.state[this.props.building]) {
+            console.log(each);
+            let restaurant = <Restaurant key={ each.name }
+                                         address={ each.address } 
+                                         categories={ each.categories }
+                                         distanceAway={ each.distanceAway }
+                                         dollarSigns={ each.dollarSigns }
+                                         img={ each.img }
+                                         name={ each.name }
+                                         timeAway={ each.timeAway }
+                                         yelpURL={ each.yelpURL }
+                             />
+
+            restaurants.push(restaurant);
+        }
+
+        return (
+            <div>
+                { restaurants }
+            </div>
+        );
+
+    }
+
+//     componentDidMount() {
+//         fetch("/restaurants.json")
+//         .then(response => {
+//             this.state = ????
+//         });
+//     }
+}
+
+
 function HBEatsSite(props) {
 
-    let restaurants = fetch("/restaurants.json?building=450")
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(data) {
-        console.log(data.info); 
-    });
+    // let restaurants = fetch("/restaurants.json?building=450")
+    // .then(function(response) {
+    //     return response.json()
+    // })
+    // .then(function(data) {
+    //     console.log(data.info); 
+    // });
 
-    console.log(restaurants);
+    // console.log(restaurants);
+
+    // {`${restaurants[0].name}`}
 
     return (
-        <Restaurant name={`${restaurants[0].name}`} />
+        <div>
+        <AllNavTabs buildings={["683", "450"]}/>
+        <RestaurantList building="683"/>
+        </div>
     );
 
 }
