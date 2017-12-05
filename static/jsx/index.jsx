6b30e7d25706de3327f7326bc526d65e5b573765
Jennifer <jennifer.gd42@gmail.com>
2017-12-04 22:40:04 -0800

    [WIP] Attempt to re-organize react files. May need to figure out how to use require or just convert to using node.

--- static/jsx/index.jsx
+++ static/jsx/index.jsx
@@ -1,22 +1,5 @@
 "use strict";
 
-class NavTab extends React.Component {
-    constructor (props) {
-        super(props);
-        this.active = this.props.active;
-    }
-
-    render () {
-        let url = "/?building=" + this.props.building;
-
-        return (
-            <li role="presentation" className={(this.active) ? "active" : ""}>
-                <a href={url}>{ this.props.building } Sutter</a>
-            </li>
-        );
-    }
-}
-
 class AllNavTabs extends React.Component {
     render () {
         return (
